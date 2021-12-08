78ef504f1b7b2588db5c19d660b6d918ff7b7115
Travis Hilton <travis.hilton@oddball.io>
2021-12-08 14:23:20 -0500

    Autofixing offense introduced by corrections

--- modules/covid_vaccine/spec/services/covid_vaccine/v0/enrollment_upload_service_spec.rb
+++ modules/covid_vaccine/spec/services/covid_vaccine/v0/enrollment_upload_service_spec.rb
@@ -32,7 +32,7 @@ describe CovidVaccine::V0::EnrollmentUploadService do
       with_settings(Settings.covid_vaccine.enrollment_service.sftp, host: host, username: username,
                                                                     password: password, port: port) do
         allow(Net::SFTP).to receive(:start).with(host, username, password: password,
-                                                                  port: port).and_yield(sftp_connection_double)
+                                                                 port: port).and_yield(sftp_connection_double)
         expect(sftp_connection_double)
           .to receive(:upload!).with(subject.io, file_name, name: file_name, progress: instance_of(handler))
         subject.upload
