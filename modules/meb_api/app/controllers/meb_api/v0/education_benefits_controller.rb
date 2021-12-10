b59e76c261d96cb8e9d81c43e8f9c7fc6b7dbb01
cesar.santiago <cesar.santiago@accenturefederal.com>
2021-12-10 17:25:57 -0500

    - Make mock fit actual response from dgib

--- modules/meb_api/app/controllers/meb_api/v0/education_benefits_controller.rb
+++ modules/meb_api/app/controllers/meb_api/v0/education_benefits_controller.rb
@@ -10,28 +10,44 @@ module MebApi
       def claimant_info
         render json:
           {
-            data:
-              {
-                'claimant':
-                  { 'claimantId': '1000000000000246', 'suffix': '', 'dateOfBirth': '1970-01-01', 'firstName': 'Herbert',
-                    'lastName': 'Hoover', 'middleName': '',
-                    'contactInfo':
-                        { 'addressLine1': '123 Martin Luther King Blvd', 'addressLine2': '', 'city': 'New Orleans',
-                          'zipcode': '70115', 'effectiveDate': '', 'zipCodeExtension': '',
-                          'emailAddress': 'test@test.com',
-                          'addressType': 'MILITARY_OVERSEAS', 'mobilePhoneNumber': '512-825-5445',
-                          'homePhoneNumber': '222-333-3333', 'countryCode': 'US', 'stateCode': 'ME' },
-                    'dobChanged': false, 'firstAndLastNameChanged': false, 'contactInfoChanged': false,
-                    'notificationMethod': 'email', 'preferredContact': 'mail' }
-              },
-            'serviceData': {
-              'beginDate': '2010-10-26T18:00:54.302Z', 'endDate': '2021-10-26T18:00:54.302Z',
-              'branchOfService': 'Army',
-              'trainingPeriods': [
-                { 'beginDate': '2018-10-26T18:00:54.302Z', 'endDate': '2019-10-26T18:00:54.302Z' }
-              ],
-              'exclusionPeriods': [{ 'beginDate': '2012-10-26T18:00:54.302Z', 'endDate': '2013-10-26T18:00:54.302Z' }],
-              'characterOfService': 'Honorable', 'reasonForSeparation': 'Expiration Term Of Service'
+            data: {
+              'id': '',
+              'type': 'meb_api_dgi_automation_claimant_responses',
+              'attributes': {
+                'claimant': {
+                  'claimantId': 1000000000000265,
+                  'suffix': '',
+                  'dateOfBirth': '1970-01-01',
+                  'firstName': 'Andrea',
+                  'lastName': 'Mitchell',
+                  'middleName': '',
+                  'notificationMethod': 'TEXT',
+                  'contactInfo': {
+                    'addressLine1': '123 Martin Luther King Blvd',
+                    'addressLine2': '',
+                    'city': 'New Orleans',
+                    'zipcode': '70115',
+                    'emailAddress': 'test@test.com',
+                    'addressType': 'MILITARY_OVERSEAS',
+                    'mobilePhoneNumber': '',
+                    'homePhoneNumber': '',
+                    'countryCode': 'US',
+                    'stateCode': 'ME'
+                  },
+                  'preferredContact': ''
+                },
+                'serviceData': [
+                  {
+                    'branchOfService': 'Air Force',
+                    'beginDate': '2010-01-01',
+                    'endDate': '2020-01-01',
+                    'characterOfService': 'Honorable',
+                    'reasonForSeparation': 'Hardship',
+                    'exclusionPeriods': [],
+                    'trainingPeriods': []
+                  }
+                ]
+              }
             }
           }
       end
