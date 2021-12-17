3e8b014218d99b8008bcf337968810d08cde0d62
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-12-17 14:41:52 -0500

    testing

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-if !Rails.env.test?
 require 'ddtrace'
 
-  Datadog.configure do |c|
-    c.use :rails, { log_injection: true }
-    c.use :sidekiq, { tag_args: true }
-  end
+# @see https://docs.datadoghq.com/tracing/setup_overview/setup/ruby
+Datadog.configure do |c|
+  c.tracer.enabled = !Rails.env.test?
+  c.service = 'vets-api'
+  c.env = Settings.vsp_environment
+  c.use :rails, { log_injection: true }
+  c.use :sidekiq, { tag_args: true }
 end
