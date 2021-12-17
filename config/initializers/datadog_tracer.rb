6d696fd716d0ee1ea808a4568dc24ce72687cf47
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-12-17 14:16:53 -0500

    start with rake, rails and sidekiq

--- config/initializers/datadog_tracer.rb
+++ config/initializers/datadog_tracer.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
+require 'ddtrace'
+
+# @see https://docs.datadoghq.com/tracing/setup_overview/setup/ruby
 Datadog.configure do |c|
+  c.use :rails, { log_injection: true }
+  c.use :sidekiq, { tag_args: true }
   c.service = 'vets-api'
   c.env = Settings.vsp_environment
 end
