8f036ecd4f2bf9d8e64e451ad52e90688f61138d
Csaba Nemeth <cnemeth@users.noreply.github.com>
2022-01-04 11:05:16 -0800

    Added FactoryBot factory DisabilityClaim

---
+++ spec/factories/disability_claim.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :disability_claim, class: SavedClaim::DisabilityCompensation::Form526AllClaim do
+    form_id { '21-526EZ' }
+
+    form do
+      {
+        standardClaim: false,
+        isVaEmployee: true,
+        privacyAgreementAccepted: true,
+        veteranFullName: {
+          first: 'Test',
+          last: 'User'
+        },
+        gender: 'F',
+        email: 'foo@foo.com',
+        veteranDateOfBirth: '1989-12-13',
+        veteranSocialSecurityNumber: '111223333',
+        veteranAddress: {
+          country: 'USA',
+          state: 'CA',
+          postalCode: '90210',
+          street: '123 Main St',
+          city: 'Anytown'
+        }
+      }.to_json
+    end
+  end
+end
