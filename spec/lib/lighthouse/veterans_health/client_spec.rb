16a053de56075d88253bac640a43dfaf9de0c98d
Julia Allen <julia.l.allen@gsa.gov>
2022-01-10 21:53:02 -0800

    add test for paginated response

--- spec/lib/lighthouse/veterans_health/client_spec.rb
+++ spec/lib/lighthouse/veterans_health/client_spec.rb
@@ -41,7 +41,9 @@ RSpec.describe Lighthouse::VeteransHealth::Client do
     let(:bearer_token_object) { double('bearer response', body: { 'access_token' => 'blah' }) }
 
     context 'valid requests' do
-      let(:generic_response) { double('lighthouse response', status: 200, body: { 'generic': 'response', 'link': [] }.as_json) }
+      let(:generic_response) do
+        double('lighthouse response', status: 200, body: { 'generic': 'response', 'link': [] }.as_json)
+      end
 
       before do
         allow(@client).to receive(:perform).and_return generic_response
@@ -107,6 +109,53 @@ RSpec.describe Lighthouse::VeteransHealth::Client do
             Lighthouse::VeteransHealth::Client
           ).to receive(:perform_get).with(medications_api_path, params_hash).and_call_original
           @client.list_resource('medications')
+        end
+
+        it 'returns the api response' do
+          expect(@client.list_resource('medications')).to eq generic_response
+        end
+
+        context 'when the response is larger than one page' do
+          let(:response_with_pages) do
+            double(
+              'lighthouse response',
+              status: 200,
+              body: { 'entry': [{ 'first': 'page' }],
+                      'link': [{ 'relation': 'next',
+                                 'url': 'https://api.fake/MedicationRequest?patient=fake&page=2' }] }.as_json
+            )
+          end
+
+          let(:next_response) do
+            double(
+              'lighthouse response',
+              status: 200,
+              body: { 'entry': [{ 'second': 'page' }],
+                      'link': [{ 'relation': 'next',
+                                 'url': 'https://api.fake/MedicationRequest?patient=fake&page=3' }] }.as_json
+            )
+          end
+
+          let(:last_response) do
+            double(
+              'lighthouse response',
+              status: 200,
+              body: { 'entry': [{ 'last': 'page' }], 'link': [] }.as_json
+            )
+          end
+
+          before do
+            allow(@client).to receive(:perform).and_return response_with_pages, next_response, last_response
+          end
+
+          it 'returns all entries from every page within the single response' do
+            expected_entries = [
+              { 'first': 'page' },
+              { 'second': 'page' },
+              { 'last': 'page' }
+            ].as_json
+            expect(@client.list_resource('medications').body['entry']).to match expected_entries
+          end
         end
       end
     end
