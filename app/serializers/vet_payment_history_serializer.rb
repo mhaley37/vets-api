eb7b928988f0f8840439882d127291f854026bca
cadibemma <chika.adibemma@adhocteam.us>
2022-01-27 17:21:36 -0500

    updated vet payment history serializer to use payment shared adapter

--- app/serializers/vet_payment_history_serializer.rb
+++ app/serializers/vet_payment_history_serializer.rb
@@ -41,14 +41,8 @@ class VetPaymentHistorySerializer < ActiveModel::Serializer
   end
 
   def process_payment(payment)
-    @formatted_payments << {
-      pay_check_dt: payment[:payment_date],
-      pay_check_amount: ActiveSupport::NumberHelper.number_to_currency(payment[:payment_amount]),
-      pay_check_type: payment[:payment_type],
-      payment_method: get_payment_method(payment),
-      bank_name: payment.dig(:address_eft, :bank_name),
-      account_number: mask_account_number(payment[:address_eft])
-    }
+    @formatted_payments <<
+      Adapters::PaymentSharedAdapter.new.process_payment(payment)
   end
 
   def process_return_payment(payment)
@@ -60,22 +54,5 @@ class VetPaymentHistorySerializer < ActiveModel::Serializer
       returned_check_type: payment[:payment_type],
       return_reason: payment[:return_payment][:return_reason]
     }
-  end
-
-  def mask_account_number(address_eft, all_but = 4, char = '*')
-    return if address_eft.blank?
-
-    account_number = address_eft[:account_number]
-    return if account_number.blank?
-
-    account_number&.gsub(/.(?=.{#{all_but}})/, char)
-  end
-
-  def get_payment_method(payment)
-    return 'Direct Deposit' if payment.dig(:address_eft, :account_number).present?
-
-    return 'Paper Check' if payment.dig(:check_address, :address_line1).present?
-
-    nil
   end
 end
