a5ce1ee15bf26d9579d6a9a817dbac157e8ce65c
cadibemma <chika.adibemma@adhocteam.us>
2022-01-31 11:54:17 -0500

    created rspec and updated controller with payment processing

--- modules/mobile/app/controllers/mobile/v0/payment_history_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/payment_history_controller.rb
@@ -9,8 +9,45 @@ module Mobile
         person = BGS::PeopleService.new(current_user).find_person_by_participant_id
         response = BGS::PaymentService.new(current_user).payment_history(person)
 
+        #
         render json: Mobile::V0::PaymentHistorySerializer.new(response)
       end
+
+      #
+      def initialize
+        @formatted_payments = []
+
+        process_all_payments(object[:payments][:payment]) if object.dig(:payments, :payment).present?
+        super
+      end
+    end
+
+    #
+    def payments
+      @formatted_payments
+    end
+
+    private
+
+    def process_all_payments(all_payments)
+      all_payments = [all_payments] if all_payments.instance_of?(Hash)
+
+      all_payments.each do |payment|
+        process_payment(payment)
+      end
+    end
+
+    def process_payment(payment)
+      @formatted_payments <<
+        Adapters::PaymentSharedAdapter.new.process_payment(payment)
+    end
+
+    def paginate(payments, validated_params)
+      payments = payments.filter do |payment|
+        payment.start_date_utc.between? validated_params[:start_date], validated_params[:end_date]
+      end
+      url = request.base_url + request.path
+      Mobile::PaginationHelper.paginate(list: payments, validated_params: validated_params, url: url)
     end
   end
 end
---
+++ modules/mobile/spec/request/payment_history_request_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../support/iam_session_helper'
+require_relative '../support/matchers/json_schema_matcher'
+
+RSpec.describe 'payment_history', type: :request do
+  include JsonSchemaMatchers
+
+end
