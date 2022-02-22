81e29f97d6b4794d877ac33bddd1da6344fb57a7
Trevor Bosaw <trevor.bosaw@oddball.io>
2022-02-21 17:04:32 -0800

    wip

--- lib/auth_logingov/service.rb
+++ lib/auth_logingov/service.rb
@@ -7,16 +7,14 @@ module AuthLogingov
   class Service < Common::Client::Base
     configuration AuthLogingov::Configuration
 
-    CLIENT_ID = Settings.logingov.client_id
-    ACR_VALUES = IAL::LOGIN_GOV_IAL2
-    TOKEN_TYPE_HINT = 'access_token'
     AUTH_PATH = 'openid_connect/authorize'
+    TOKEN_PATH = 'api/openid_connect/token'
     RESPONSE_TYPE = 'code'
     PROMPT = 'select_account'
     SCOPE = 'profile email openid social_security_number'
-    REDIRECT_URI = Settings.logingov.redirect_uri
     GRANT_TYPE = 'authorization_code'
-    LOGINGOV_CLIENT_ASSERTION_TYPE = 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+    CLIENT_ASSERTION_TYPE = 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+    CLIENT_ASSERTION_EXPIRATION_SECONDS = 1000
 
     def render_auth
       renderer = ActionController::Base.renderer
@@ -26,11 +24,11 @@ module AuthLogingov
                         url: auth_url,
                         params:
                         {
-                          acr_values: ACR_VALUES,
-                          client_id: CLIENT_ID,
+                          acr_values: IAL::LOGIN_GOV_IAL2,
+                          client_id: Settings.logingov.client_id,
                           nonce: SecureRandom.hex,
                           prompt: PROMPT,
-                          redirect_uri: REDIRECT_URI,
+                          redirect_uri: Settings.logingov.redirect_uri,
                           response_type: RESPONSE_TYPE,
                           scope: SCOPE,
                           state: state
@@ -55,26 +53,26 @@ module AuthLogingov
     end
 
     def token_url
-      "#{Settings.logingov.oauth_url}/api/openid_connect/token"
+      "#{Settings.logingov.oauth_url}/#{TOKEN_PATH}"
     end
 
     def token_params(code)
       {
         grant_type: GRANT_TYPE,
         code: code,
-        client_assertion_type: LOGINGOV_CLIENT_ASSERTION_TYPE,
+        client_assertion_type: CLIENT_ASSERTION_TYPE,
         client_assertion: client_assertion_jwt
       }.to_json
     end
 
     def client_assertion_jwt
       jwt_payload = {
-        iss: CLIENT_ID,
-        sub: CLIENT_ID,
+        iss: Settings.logingov.client_id,
+        sub: Settings.logingov.client_id,
         aud: token_url,
         jti: SecureRandom.hex,
-        nonce: SecureRandom.hex,
-        exp: Time.now.to_i + 1000
+        nonce: nonce,
+        exp: Time.now.to_i + CLIENT_ASSERTION_EXPIRATION_SECONDS
       }
       JWT.encode(jwt_payload, private_key, 'RS256')
     end
@@ -83,9 +81,12 @@ module AuthLogingov
       OpenSSL::PKey::RSA.new(File.open(Settings.logingov.client_key_path))
     end
 
-    # TODO: Put stuff in state
     def state
-      SecureRandom.hex
+      @state ||= SecureRandom.hex
+    end
+
+    def nonce
+      @nonce ||= SecureRandom.hex
     end
   end
 end
