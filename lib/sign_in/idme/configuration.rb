76c34fdb81506d9bab33ab86d85014671ae9a8c6
Josh Scanish <josh.scanish@oddball.io>
2022-03-04 15:33:05 -0500

    Set up some basic configurations

--- lib/sign_in/idme/configuration.rb
+++ lib/sign_in/idme/configuration.rb
@@ -5,5 +5,24 @@ require 'common/client/middleware/logging'
 
 module SignIn::Idme
   class Configuration < Common::Client::Configuration::REST
+    def base_path
+      Settings.idme.oauth_url
+    end
+
+    def client_id
+      Settings.idme.client_id
+    end
+
+    def redirect_uri
+      Settings.idme.redirect_uri
+    end
+
+    def client_key_path
+      Settings.idme.client_key_path
+    end
+
+    def client_cert_path
+      Settings.idme.client_cert_path
+    end
   end
 end
