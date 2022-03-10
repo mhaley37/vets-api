806a63556525005801ac08aefba1cc51dfd8745b
John Bramley <bramleyjl@gmail.com>
2022-03-10 09:48:32 -0700

    rubocop fixes

--- spec/lib/sign_in/logingov/service_spec.rb
+++ spec/lib/sign_in/logingov/service_spec.rb
@@ -30,8 +30,8 @@ describe SignIn::Logingov::Service do
   end
   let(:success_callback_url) { 'http://localhost:3001/auth/login/callback?type=logingov' }
   let(:failure_callback_url) { 'http://localhost:3001/auth/login/callback?auth=fail&code=007' }
-  let(:ssl_key) { OpenSSL::PKey::RSA.new(File.read("spec/fixtures/sign_in/logingov.key")) }
-  let(:ssl_cert) { OpenSSL::X509::Certificate.new(File.read("spec/fixtures/sign_in/logingov.crt")) }
+  let(:ssl_key) { OpenSSL::PKey::RSA.new(File.read('spec/fixtures/sign_in/logingov.key')) }
+  let(:ssl_cert) { OpenSSL::X509::Certificate.new(File.read('spec/fixtures/sign_in/logingov.crt')) }
 
   before do
     allow_any_instance_of(SignIn::Logingov::Configuration).to receive(:ssl_key).and_return(ssl_key)
