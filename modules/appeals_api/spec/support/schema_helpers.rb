87e069ca76d0bc4269c3258d1cc89ee3d27241ef
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2022-03-18 15:04:51 -0600

    Adds helper method.

--- modules/appeals_api/spec/support/schema_helpers.rb
+++ modules/appeals_api/spec/support/schema_helpers.rb
@@ -15,4 +15,12 @@ module SchemaHelpers
       )
     )
   end
+
+  def self.schema_max_lengths(file, schema, _schema_version = 'V2')
+    JSONSchemer.schema(schema,
+                       after_property_validation: proc do |data, property, property_schema, _parent|
+                         data[property] = 'W' * property_schema['maxLength'] if property_schema['maxLength']
+                       end).valid?(file.form_data)
+    file.form_data
+  end
 end
