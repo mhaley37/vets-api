393eb0ac19763fec2c072f39219db4ed33a2367c
Kris Pethtel <kris.pethtel@gmail.com>
2022-03-22 12:54:25 -0400

    wip

--- modules/mobile/app/services/mobile/v0/sessions/fetch_token.rb
+++ modules/mobile/app/services/mobile/v0/sessions/fetch_token.rb
@@ -1,22 +1,17 @@
 require 'capybara'
+require 'pry'
 
 TOKEN_URL = 'https://va-mobile-cutter.herokuapp.com'
 
 session = Capybara::Session.new(:selenium)
 session.visit TOKEN_URL
 session.click_link('Please Login')
-wait_until_scope_exists '.btn_idme3' do
-  session.click_link()
-end
-#   fill_in 'Email', :with => 'foo@bar.com' 
-#   fill_in 'Password', :with => 'secret'
+sleep 2 # not ideal but the first thing i've found that handles the redirects
+session.click_button('Sign in with ID.me')
+sleep 2
+session.click_button('Accept')
+sleep 2
+session.fill_in 'user_email', :with => 'foo@bar.com' 
+session.fill_in 'user_password', :with => 'secret'
 #   click_on 'Sign in'
-# end
-binding.pry
-
-# session.find('#sign-in-email').set(ENV.fetch('EMAIL'))
-# session.find(:xpath, '//input[@type="password"]').set(ENV.fetch('PASSWORD'))
-# session.find('.hw-btn-primary').click
-# session.all('.confirmation-code').each do |code|
-#   puts code.text
 # end
