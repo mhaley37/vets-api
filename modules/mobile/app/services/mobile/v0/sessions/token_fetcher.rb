a9da72f33d73f2588d20e1c17e6ac66f94c84194
Kris Pethtel <kris.pethtel@gmail.com>
2022-03-23 17:06:11 -0400

    remove unnecessary sleeps

--- modules/mobile/app/services/mobile/v0/sessions/token_fetcher.rb
+++ modules/mobile/app/services/mobile/v0/sessions/token_fetcher.rb
@@ -6,34 +6,30 @@ class TokenFetcher
   TOKEN_URL = 'https://va-mobile-cutter.herokuapp.com'
   USERS_FILE = 'modules/mobile/app/services/mobile/v0/sessions/login_users.yml'
 
-  attr_reader :user_name, :token
+  attr_reader :user_name, :token, :session
 
   def initialize(user_name)
     @user_name = user_name
+    Capybara.default_max_wait_time = 5
+    @session = Capybara::Session.new(:selenium)
   end
 
   def fetch_token
-    session = Capybara::Session.new(:selenium)
     session.visit TOKEN_URL
     session.click_link('Please Login')
-    # all of these sleeps should not be necessary but it's the first thing i've found that works
-    sleep 3
+    sleep 3 # this sleep appears to be necessary for dealing with the redirect
     session.click_button('Sign in with ID.me')
-    sleep 3
     session.click_button('Accept')
-    sleep 3
     session.fill_in 'user_email', with: user['email']
     session.fill_in 'user_password', with: user['password']
     session.click_button('Sign in to ID.me')
-    sleep 3
     session.click_button('Continue')
-    sleep 3
     session.click_button('Continue')
-    sleep 5
     session.click_button('Accept')
     session.click_button('Auth')
     token_text = session.find('div', text: /Access Token: ?/i).text
     @token = token_text.split[2]
+    puts "found token: #{token}"
   end
 
   def copy_token_to_clipboard
