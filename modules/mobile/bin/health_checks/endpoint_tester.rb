4bd4ef27249fe282274526861a7a7dedcb26508b
Kris Pethtel <kris.pethtel@gmail.com>
2022-03-28 18:07:43 -0400

    better error outputting

--- modules/mobile/bin/health_checks/endpoint_tester.rb
+++ modules/mobile/bin/health_checks/endpoint_tester.rb
@@ -14,7 +14,7 @@ class EndpointTester < Thor
   option :test_name
   def run_tests
     @users = {}
-    @results = { success: 0, failure: 0 }
+    @results = {}
     files = Dir["#{TEST_DATA_DIR}/**/*.yaml"]
     files.each do |f|
       test_data = YAML.safe_load(File.read(f))
@@ -48,6 +48,61 @@ class EndpointTester < Thor
     validate_response(data, response)
   end
 
+  def validate_response(expected_data, response)
+    test_name = expected_data['name']
+    @results[test_name] = []
+
+    expected_status = expected_data.dig('response', 'status')
+    correct_status_received = expected_status == response.status
+
+    if correct_status_received
+      received_data = JSON.parse(response.body)['data']
+
+      count = expected_data.dig('response', 'count')
+      add_error(test_name, 'count', count, received_data.count) if count && count != received_data.count
+
+      expected_data = expected_data.dig('response', 'data')
+      # ensuring that both data sets are hashes to avoid having a special case when data is an array
+      compare_data(test_name, { 'data' => expected_data }, { 'data' => received_data }) if expected_data
+    else
+      add_error(test_name, 'status', expected_status, response.status)
+    end
+
+    @results[test_name].empty? ? print('.'.green) : print('.'.red)
+  end
+
+  def compare_data(test_name, expected, received)
+    expected.each do |k, v|
+      case v
+      when Hash
+        compare_data(test_name, v, received[k])
+      when Array
+        v.each_with_index do |array_item, i|
+          compare_data(test_name, array_item, received[k][i])
+        end
+      else
+        add_error(test_name, k, v, received[k]) unless v == received[k]
+      end
+    end
+  end
+
+  def add_error(test_name, descriptor, expected, received)
+    @results[test_name] << "#{descriptor}: expected #{expected}, received #{received}"
+  end
+
+  def process_results
+    successes, failures = @results.partition { |_, v| v.empty? }
+
+    puts
+    puts "Successful tests: #{successes.count}".green
+    failures.each do |name, messages|
+      puts "Test #{name} failed with errors:".red
+      messages.each { |message| puts "\t#{message.red}" }
+    end
+    abort("Failed tests: #{failures.count}".red) if failures.any?
+    puts 'All tests passing'.green
+  end
+
   def client(user_name)
     return @users[user_name] if @users[user_name]
 
@@ -61,57 +116,6 @@ class EndpointTester < Thor
     fetcher = TokenFetcher.new(user_name)
     fetcher.fetch_token
     fetcher.token
-  end
-
-  def validate_response(expected_data, response)
-    status = expected_data.dig('response', 'status')
-    correct_status_received = equal?('status', status, response.status)
-
-    if correct_status_received
-      received_data = JSON.parse(response.body)['data']
-
-      count = expected_data.dig('response', 'count')
-      equal?('count', count, received_data.count) if count
-
-      expected_data = expected_data.dig('response', 'data')
-      # ensuring that both data sets are hashes to avoid having a special case when data is an array
-      compare_data({ 'data' => expected_data }, { 'data' => received_data }) if expected_data
-    end
-  end
-
-  def compare_data(expected, received)
-    expected.each do |k, v|
-      case v
-      when Hash
-        compare_data(v, received[k])
-      when Array
-        v.each_with_index do |array_item, i|
-          compare_data(array_item, received[k][i])
-        end
-      else
-        equal?(k, v, received[k])
-      end
-    end
-  end
-
-  def equal?(descriptor, expected, observed)
-    if expected == observed
-      @results[:success] += 1 # this should be done elsewhere
-      print '.'.green
-      true
-    else
-      @results[:failure] += 1
-      puts
-      puts "Incorrect #{descriptor}. Expected #{expected}, received #{observed}".red
-      false
-    end
-  end
-
-  def process_results
-    puts
-    puts "Successful tests: #{@results[:success]}".green
-    abort("Failed tests: #{@results[:failure]}".red) if @results[:failure].positive?
-    puts 'All tests passing'.green
   end
 end
 
