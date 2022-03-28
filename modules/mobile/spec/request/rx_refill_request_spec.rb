55bb2d1df9d5a2a993a37f3390379825bd4e13c1
andrewherzberg <aherzberg5@gmail.com>
2022-03-28 12:10:12 -0700

    make rx changes

--- modules/mobile/spec/request/rx_refill_request_spec.rb
+++ modules/mobile/spec/request/rx_refill_request_spec.rb
@@ -6,42 +6,39 @@ require 'support/shared_examples_for_mhv'
 require_relative '../support/iam_session_helper'
 require_relative '../support/matchers/json_schema_matcher'
 
-
 RSpec.describe 'rx_refill', type: :request do
   include JsonSchemaMatchers
   include Rx::ClientHelpers
 
-  let(:va_patient) { true }
+  #let(:va_patient) { true }
   let(:mhv_account_type) { 'Premium' }
-  #let(:current_user) do
+  # let(:current_user) do
   #  build(:user, :mhv, authn_context: LOA::IDME_LOA3_VETS, va_patient: va_patient, mhv_account_type: mhv_account_type, sign_in: { service_name: 'idme' })
-  #end
+  # end
+
+  before(:all) do
+    @original_cassette_dir = VCR.configure(&:cassette_library_dir)
+    VCR.configure { |c| c.cassette_library_dir = 'modules/mobile/spec/support/vcr_cassettes' }
+  end
+
+  after(:all) { VCR.configure { |c| c.cassette_library_dir = @original_cassette_dir } }
 
   before do
-
     allow_any_instance_of(MHVAccountTypeService).to receive(:mhv_account_type).and_return(mhv_account_type)
     allow(Rx::Client).to receive(:new).and_return(authenticated_client)
     current_user = build(:iam_user, :idme)
 
     iam_sign_in(current_user)
-
   end
-
 
   describe 'GET /mobile/v0/rx-refill/get_full_rx_history' do
     context 'with a valid 200 response' do
       it 'matches the get_full_rx_history schema' do
-          VCR.use_cassette('modules/mobile/spec/support/vcr_cassettes/rx_refill/prescriptions/gets_a_list_of_all_prescriptions') do
-
-            get '/mobile/v0/rx-refill/full_rx_history', headers: iam_headers
-
+        VCR.use_cassette('rx_refill/prescriptions/gets_a_list_of_all_prescriptions') do
+          get '/mobile/v0/rx-refill/full_rx_history', headers: iam_headers
         end
-        puts 'here1'
-        puts response.body
+        binding.pry
         expect(response).to have_http_status(:ok)
-
-        #expect(JSON.parse(response.body)).to eq(expected_response)
-        # expect(response.body).to match_json_schema('disability_rating_response')
       end
     end
   end
