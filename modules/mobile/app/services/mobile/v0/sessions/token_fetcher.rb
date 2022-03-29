2ac86d250ad771623e34a76aa6cd42a97cfbf332
Kris Pethtel <kris.pethtel@gmail.com>
2022-03-29 12:44:25 -0400

    wip

--- modules/mobile/app/services/mobile/v0/sessions/token_fetcher.rb
+++ modules/mobile/app/services/mobile/v0/sessions/token_fetcher.rb
@@ -8,7 +8,7 @@ class TokenFetcher
   TOKEN_URL = 'https://va-mobile-cutter.herokuapp.com'
   USERS_FILE = ENV['STAGING_TEST_USERS']
 
-  attr_reader :user, :token, :session
+  attr_reader :token
 
   def initialize(user_name)
     set_user(user_name)
@@ -17,19 +17,19 @@ class TokenFetcher
   end
 
   def fetch_token
-    session.visit TOKEN_URL
-    session.click_link('Please Login')
+    @session.visit TOKEN_URL
+    @session.click_link('Please Login')
     sleep 3 # this sleep appears to be necessary for dealing with the redirect
-    session.click_button('Sign in with ID.me')
-    session.click_button('Accept')
-    session.fill_in 'user_email', with: user['email']
-    session.fill_in 'user_password', with: user['password']
-    session.click_button('Sign in to ID.me')
-    session.click_button('Continue')
-    session.click_button('Continue')
-    session.click_button('Accept')
-    session.click_button('Auth')
-    token_text = session.find('div', text: /Access Token: ?/i).text
+    @session.click_button('Sign in with ID.me')
+    @session.click_button('Accept')
+    @session.fill_in 'user_email', with: @user['email']
+    @session.fill_in 'user_password', with: @user['password']
+    @session.click_button('Sign in to ID.me')
+    @session.click_button('Continue')
+    @session.click_button('Continue')
+    @session.click_button('Accept')
+    @session.click_button('Auth')
+    token_text = @session.find('div', text: /Access Token: ?/i).text
     @token = token_text.split[2]
   end
 
@@ -37,13 +37,11 @@ class TokenFetcher
     IO.popen('pbcopy', 'w') { |pipe| pipe.puts token }
   end
 
-  # this should not be in this file
   def set_user(user_name)
     @user = users_data[user_name]
     raise 'User not found' unless @user
   end
 
-  # do this by environment variable instead
   def users_data
     YAML.safe_load(File.read(USERS_FILE))
   end
