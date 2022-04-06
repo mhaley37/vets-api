70152fdd55addc88dd457dfcb7ba54b5f03c6ec5
zach lewis <zlewis@clarityinnovates.com>
2022-04-06 16:45:38 -0500

    add request to other service

--- app/controllers/srestatus_controller.rb
+++ app/controllers/srestatus_controller.rb
@@ -1,7 +1,14 @@
+require 'faraday'
+require 'json'
+
 class SrestatusController < ApplicationController
     before_action :authenticate, only: [:welcome]
     def index
-      render json: { services: [ 'srv01': { 'status': 'ok', 'msg': 'none' }, 'srv02': { 'status': 'failed', 'msg': 'Down For Repair' } ],
-       incidents: [ 'srv02-20220406120000': { 'type': 'network', 'msg': 'TIC Error' } ]}
+      url = 'https://icanhazdadjoke.com/'
+      response = Faraday.get(url, {a: 1}, {'Accept' => 'application/json'})
+      joke_object = JSON.parse(response.body, symbolize_names: true)
+      puts joke_object
+      render json: { services: [ 'srv01': { 'status': 'ok', 'msg': joke_object }, 'srv02': { 'status': 'failed', 'msg': 'Down For Repair' } ],
+        incidents: [ 'srv02-20220406120000': { 'type': 'network', 'msg': 'TIC Error' } ]}
     end
 end
