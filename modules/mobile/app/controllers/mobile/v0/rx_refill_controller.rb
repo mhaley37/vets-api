61da82f791c66e36d5c93b40183b1341db23fdd9
andrewherzberg <aherzberg5@gmail.com>
2022-04-07 13:31:01 -0700

    remove non-rx history content

--- modules/mobile/app/controllers/mobile/v0/rx_refill_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/rx_refill_controller.rb
@@ -11,33 +11,6 @@ module Mobile
         render json: Mobile::V0::RxRefillHistorySerializer.new(@current_user.id, client.get_history_rxs)
       end
 
-      def get_tracking_history
-        render json: Mobile::V0::RxRefillTrackerHistorySerializer.new(
-          @current_user.id,
-          client.get_tracking_history_rx(params[:id])
-        )
-      end
-
-      def post_refill
-        client.post_refill_rx(params[:id])
-
-        head :no_content
-      end
-
-      def get_preferences
-        render json: Mobile::V0::RxRefillPreferencesSerializer.new(client.get_preferences)
-      end
-
-      def post_preferences
-        render json: Mobile::V0::RxRefillPreferencesSerializer.new(
-          client.post_preferences(params.permit(:rx_flag, :email_address))
-        )
-      end
-
-      def get_prescription
-        render json: Mobile::V0::RxRefillPrescriptionSerializer.new(client.get_rx(params[:id]))
-      end
-
       private
 
       def client
