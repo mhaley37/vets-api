6b12ec44f9cf622aaf03402b4c14ebf29fc6395a
Cam Bui <cam.bui@thoughtworks.com>
2022-05-04 14:26:08 -0700

    DHP-154 Fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,12 @@
 
 # DHP Seeds; Remove before merge to master
 
-Device.create(name: 'Vendor 1')
-Device.create(name: 'Vendor 2')
-VeteranDeviceRecord.create(icn: 'user1', device_id: 1, active: true)
-VeteranDeviceRecord.create(icn: 'user1', device_id: 2, active: false)
+device1 = Device.create(name: 'Vendor 11', key:'vendor-11')
+device1_id = device1.id
+device2 = Device.create(name: 'Vendor 22', key:'vendor-22')
+device2_id = device2.id
+
+VeteranDeviceRecord.create(icn: '8f683e74-cb11-4d11-94a3-ea97ad8723db', device_id: device1_id , active: true)
+VeteranDeviceRecord.create(icn: '8f683e74-cb11-4d11-94a3-ea97ad8723db', device_id: device2_id, active: false)
 
 
