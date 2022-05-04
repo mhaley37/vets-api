c2a539d52145d6d9ea8fb9fcda08799e1e49d765
Yoni Doron-Peters <jonathan.d@thoughtworks.com>
2022-05-03 22:55:41 -0600

    DHP-154 Refactor device and record serialization

--- modules/dhp_connected_devices/app/controllers/dhp_connected_devices/veteran_device_records_controller.rb
+++ modules/dhp_connected_devices/app/controllers/dhp_connected_devices/veteran_device_records_controller.rb
@@ -4,8 +4,9 @@ module DhpConnectedDevices
   class VeteranDeviceRecordsController < ApplicationController
 
     def index
-      active_devices = VeteranDeviceRecord.active_devices(@current_user)
-      inactive_devices = Device.where.not(id: active_devices.map(&:device_id))
+      veteran_device_records = VeteranDeviceRecord.active_devices(@current_user)
+      active_devices = Device.where(id: veteran_device_records.map(&:device_id)).select(:key, :name)
+      inactive_devices = Device.where.not(id: veteran_device_records.map(&:device_id)).select(:key, :name)
       render json: VeteranDeviceRecordSerializer.serialize(active_devices, inactive_devices)
     end
   end
--- modules/dhp_connected_devices/app/serializers/veteran_device_record_serializer.rb
+++ modules/dhp_connected_devices/app/serializers/veteran_device_record_serializer.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 class VeteranDeviceRecordSerializer
-  def self.serialize(device_records, inactive_devices)
-    active_devices = Device.where(id: device_records.map(&:device_id)).select(:key, :name)
+  def self.serialize(active_devices, inactive_devices)
     {
       devices:
-        active_devices
-          .map { |device| device.as_json(:except => :id).merge({ authUrl: "/#{device.key}", disconnectUrl: "/#{device.key}/disconnect", connected: true }) } +
-          inactive_devices.select(:key, :name)
-                          .map { |device| device.as_json(:except => :id).merge({ authUrl: "/#{device.key}", disconnectUrl: "/#{device.key}/disconnect", connected: false }) }
+        DeviceSerializer.serialize(active_devices, true) +
+          DeviceSerializer.serialize(inactive_devices, false)
     }
   end
 end
