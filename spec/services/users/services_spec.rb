bb7be926a496fa5185db6f85ddcd2798a9916277
Patrick Vinograd <patrick@adhocteam.us>
2022-05-04 14:17:29 -0700

    Removes beta_registration API and mix-in
    The beta_registration mechanism predates the introduction of feature
    flipper and is not used by any frontend applications.

--- spec/services/users/services_spec.rb
+++ spec/services/users/services_spec.rb
@@ -7,13 +7,6 @@ RSpec.describe Users::Services do
     subject { Users::Services.new(user).authorizations }
 
     let(:user) { build :user, :loa3 }
-    let(:beta_feature_preferences) { 'preferences' }
-    let(:beta_feature_admin) { 'admin' }
-
-    before do
-      create :beta_registration, user_uuid: user.uuid, feature: beta_feature_preferences
-      create :beta_registration, user_uuid: user.uuid, feature: beta_feature_admin
-    end
 
     it 'returns an array of services authorized to the initialized user', :aggregate_failures do
       expect(subject.class).to eq Array
@@ -30,8 +23,6 @@ RSpec.describe Users::Services do
           'form-prefill',
           'identity-proofed',
           'vet360',
-          beta_feature_preferences,
-          beta_feature_admin
         ]
       )
     end
@@ -48,8 +39,6 @@ RSpec.describe Users::Services do
             'user-profile',
             'form-save-in-progress',
             'form-prefill',
-            beta_feature_preferences,
-            beta_feature_admin
           ]
         )
       end
